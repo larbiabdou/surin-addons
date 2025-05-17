923097bb3151c55b502ff402d9a94993b7f25ed2
larbiabdou <larbiabdou36@gmail.com>
2025-05-17 16:40:56 +0100

    add module maintenance repair

--- sh_picking_weight/models/stock_move.py
+++ sh_picking_weight/models/stock_move.py
@@ -6,19 +6,20 @@ from odoo import models,fields, api
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    sh_weight = fields.Float(string="Weight", readonly=True) 
+    sh_weight = fields.Float(string="Weight", readonly=True, digits=(16, 2))
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move.line'
 
-    sh_weight = fields.Float(string="Weight")
+    sh_weight = fields.Float(string="Weight", digits=(16, 2))
 
     def _get_aggregated_product_quantities(self, **kwargs):
         aggregated_move_lines = super()._get_aggregated_product_quantities(**kwargs)
         print("\n\n\n\n\naggregated_move_lines", aggregated_move_lines)
         for aggregated_move_line in aggregated_move_lines:
             sh_weight = aggregated_move_lines[aggregated_move_line]['product'].weight * \
-                aggregated_move_lines[aggregated_move_line]['quantity']
-            aggregated_move_lines[aggregated_move_line]['sh_weight'] = sh_weight
+                        aggregated_move_lines[aggregated_move_line]['quantity']
+            # Arrondir à deux chiffres après la virgule
+            aggregated_move_lines[aggregated_move_line]['sh_weight'] = round(sh_weight, 2)
         return aggregated_move_lines
